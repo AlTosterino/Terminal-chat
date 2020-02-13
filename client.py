e987833055079af8dcc31bcbc032cee216dca543
AlTosterino <altosterino@gmail.com>
2020-02-13 16:10:56 +0100

    Add disconnecting messages, and exit function

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import asyncio
 import socketio
 import sys
+import os
 from colorama import init, Fore, Style
 from time import sleep
 from threading import Thread
@@ -22,9 +23,21 @@ async def connect_error():
 async def disconnect():
     print("I'm disconnected!")
 
+def exit():
+    loop.call_soon_threadsafe(loop.stop)
+    os._exit(0)
+
 async def connect_to_server():
-    await sio.connect('http://localhost:5000')
-    await sio.wait()
+    try:
+        await sio.connect('http://localhost:5000')
+    except socketio.exceptions.ConnectionError:
+        print('Disconnecting...')
+        exit()
+    try:
+        await sio.wait()
+    except AttributeError:
+        print('Disconnecting...')
+        exit()
 
 @sio.event
 async def message(data):
@@ -40,5 +53,9 @@ if __name__ == '__main__':
     t.start()
     sleep(1)
     while True:
-        x = input(Fore.GREEN + 'You: ' + Style.RESET_ALL)
-        asyncio.run_coroutine_threadsafe(emit(x), loop)
+        try:
+            x = input(Fore.GREEN + 'You: ' + Style.RESET_ALL)
+            asyncio.run_coroutine_threadsafe(emit(x), loop)
+        except KeyboardInterrupt:
+            print('Disconnecting...')
+            exit()
